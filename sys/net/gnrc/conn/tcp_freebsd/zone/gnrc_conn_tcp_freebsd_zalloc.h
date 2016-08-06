4e065558cfbd4b8e27eadb3ab64e9ab82cbb5d8f
Sam Kumar <samkumar99@gmail.com>
2016-08-05 20:59:00 -0700

    Port of FreeBSD TCP implementation for gnrc, with TinyOS-style event scheduler and condition variable implementation I chose to just use the SOCK implementation of posix_sockets.c, which will break the TCP_FREEBSD since it is implemented assuming CONN. So nothing will work right now... a fix is coming soon however
    The original commits are:
    
    Added FreeBSD TCP code and very basic integration into GNRC (doesn't compile yet)
    
    Add dummy method stubs and made modifications so that the code compiles; still doesn't link
    
    Fix some linker problems
    
    Fixed module definitions so that the code compiles
    
    Implement receipt of packets from lower layer
    
    Modify FreeBSD code to send packets using pktsnips
    
    Use consistent errno.h file
    
    Some more progress
    
    Implemented TinyOS-style scheduler (compiles, but not tested yet)
    
    Stub the remaining functions
    
    Debug task scheduler
    
    Start working on socket allocator
    
    Implement remaining functions for socket allocator
    
    Define external API in header file for TCP
    
    Delete unnecessary file and add some (coarse) synchronization to the TCP module
    
    Check the checksum for received segments
    
    Reorganize the code a bit, and fix bugs
    
    Fix bugs in TCP segment reception
    
    Fix bugs interfering with SYN-ACK retransmission
    
    Start implementing conn API for TCP
    
    Implement condition variable
    
    More work on conn API
    
    More work on conn interface
    
    Add memmgr-based zone allocator for TCP conn API
    
    Use union-based conn tcp structure
    
    Implemented all of conn except for send
    
    Preliminary implementation of send capability in conn API
    
    Fix memory leak
    
    Tie conn API logic into posix_sockets
    
    Fix bugs, and accept on ACK instead of SYN-ACK
    
    Got send/recv somewhat working
    
    Fix some bugs
    
    Update to new APIs after rebasing
    
    Fix race condition in initialization of TCP packet loop
    
    Prevent radio from going to sleep after send
    
    Fix some resource bugs in TCP Conn API implementation
    
    Fix bug in RIOT that prevents socket 0 from working properly
    
    Fix more bugs

---
+++ sys/net/gnrc/conn/tcp_freebsd/zone/gnrc_conn_tcp_freebsd_zalloc.h
@@ -0,0 +1,45 @@
+/*
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef GNRC_CONN_TCP_FREEBSD_ZALLOC_H_
+#define GNRC_CONN_TCP_FREEBSD_ZALLOC_H_
+
+#include "memmgr.h"
+
+#define ENABLE_DEBUG (0)
+
+#include "debug.h"
+
+static bool initialized = false;
+
+static inline void conn_tcp_freebsd_zone_init(void)
+{
+    if (!initialized) {
+        initialized = true;
+        memmgr_init();
+    }
+}
+
+static inline void* conn_tcp_freebsd_zalloc(unsigned long numbytes) {
+    if (numbytes == 0) {
+        return NULL;
+    }
+    conn_tcp_freebsd_zone_init();
+    void* p = memmgr_alloc(numbytes);
+    DEBUG("Allocating %lu bytes: %p\n", numbytes, p);
+    return p;
+}
+
+static inline void conn_tcp_freebsd_zfree(void* ptr) {
+    if (ptr == NULL) {
+        return;
+    }
+    assert(initialized);
+    DEBUG("Freeing %p\n", ptr);
+    memmgr_free(ptr);
+}
+
+#endif
