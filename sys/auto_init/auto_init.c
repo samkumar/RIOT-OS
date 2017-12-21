757a55860c58e89b152371c7b0dcd033e739305d
Sam Kumar <samkumar99@gmail.com>
2017-12-20 16:11:25 -0800

    sys/vfs: reserve file descriptors for stdin, stdout, and stderr
    Previously, this was done in uart_stdio. However, this does not work when
    rtt_stdio is used instead. By moving the code into the VFS module, it can be
    used regardless of the mechanism to write characters to the screen.
    
    Adding a vfs_init() function also allows us to initialize the_vfs_open_files
    array, which previously was not being done.

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -96,6 +96,10 @@
 #include "net/gnrc/ipv6/nib.h"
 #endif
 
+#ifdef MODULE_VFS
+#include "vfs.h"
+#endif
+
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
@@ -112,6 +116,10 @@ void auto_init(void)
 #ifdef MODULE_RTC
     DEBUG("Auto init rtc module.\n");
     rtc_init();
+#endif
+#ifdef MODULE_VFS
+    DEBUG("Auto init VFS module.\n")
+    vfs_init();
 #endif
 #ifdef MODULE_SHT11
     DEBUG("Auto init SHT11 module.\n");
